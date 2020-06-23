84a54d64e24f2dbbb4279fc051be35fb5113ecea
WinPooh32 <winpooh32@yandex.ru>
2020-06-23 14:05:35 +1000

    Add saving/loading torrents to/from db

--- app/app.go
+++ app/app.go
@@ -1,17 +1,24 @@
 package app
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"sync"
 
-	anacrolixlog "github.com/anacrolix/log"
 	"github.com/anacrolix/torrent"
-	"github.com/anacrolix/torrent/mse"
-	"golang.org/x/time/rate"
+	"github.com/boltdb/bolt"
+	"github.com/rs/zerolog/log"
 
 	"github.com/WinPooh32/peerstohttp/settings"
+
+	"github.com/anacrolix/torrent/metainfo"
+)
+
+const (
+	dbName       = ".app.bolt.db"
+	dbBucketInfo = "torrent_info"
 )
 
 // TODO add torrent management(disk cache size control, start/stop and etc.).
@@ -21,86 +28,120 @@ type App struct {
 	torrents map[string]*torrent.Torrent
 	mu       sync.RWMutex
 
+	db *bolt.DB
+
 	// Path to temporary data folder.
 	tmp string
+	cwd string
 }
 
 func New(service *settings.Settings) (*App, error) {
 	var err error
 	var tmp string
-	var cfg *torrent.ClientConfig = torrent.NewDefaultClientConfig()
+	var cwd string
 
-	// Bind port.
-	cfg.ListenPort = *service.TorrPort
+	var client *torrent.Client
+	var store *bolt.DB
 
-	// Download directory.
+	// Working directory.
 	if *service.DownloadDir == "" {
 		tmp, err = ioutil.TempDir("", "peerstohttp")
 		if err != nil {
-			return nil, fmt.Errorf("create temp folder: %w", err)
+			return nil, fmt.Errorf("create temp. folder: %w", err)
 		}
-
-		cfg.DataDir = tmp
+		cwd = tmp
 	} else {
-		cfg.DataDir = *service.DownloadDir
+		cwd = *service.DownloadDir
 	}
 
-	// Rate limits.
-	const kib = 1 << 10
-
-	if *service.DownloadRate != 0 {
-		cfg.DownloadRateLimiter = limit(*service.DownloadRate * kib)
-	}
-
-	if *service.UploadRate != 0 {
-		cfg.UploadRateLimiter = limit(*service.UploadRate * kib)
-	}
-
-	cfg.EstablishedConnsPerTorrent = *service.MaxConnections
-	cfg.TorrentPeersLowWater = *service.MaxConnections
-
-	cfg.NoDHT = *service.NoDHT
-	cfg.DisableUTP = *service.NoUTP
-	cfg.Seed = true
-
-	// Header obfuscation.
-	cfg.HeaderObfuscationPolicy = torrent.HeaderObfuscationPolicy{
-		Preferred:        true,
-		RequirePreferred: *service.ForceEncryption,
-	}
-	// Force encryption.
-	if *service.ForceEncryption {
-		cfg.CryptoProvides = mse.CryptoMethodRC4
-	}
-
-	// Torrent debug.
-	cfg.Debug = false
-	if !*service.TorrentDebug {
-		cfg.Logger = anacrolixlog.Discard
-	}
-
-	cfg.DefaultRequestStrategy = torrent.RequestStrategyFastest()
-
-	client, err := torrent.NewClient(cfg)
+	client, err = p2p(service, cwd)
 	if err != nil {
 		return nil, fmt.Errorf("new torrent client: %w", err)
 	}
 
-	return &App{
+	store, err = db(fmt.Sprintf("%s/%s", cwd, dbName))
+	if err != nil {
+		return nil, fmt.Errorf("new db: %w", err)
+	}
+
+	var app = &App{
 		torrents: map[string]*torrent.Torrent{},
 		client:   client,
+		db:       store,
 		tmp:      tmp,
-	}, nil
+		cwd:      cwd,
+	}
+
+	err = app.Load()
+	if err != nil {
+		return nil, fmt.Errorf("load app state from db: %w", err)
+	}
+
+	log.Info().Msg("app loaded")
+
+	return app, nil
+}
+
+func (app *App) Load() error {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+
+	return app.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(dbBucketInfo))
+
+		return b.ForEach(func(k, v []byte) error {
+			var err error
+			var mi *metainfo.MetaInfo
+			var t *torrent.Torrent
+
+			mi, err = metainfo.Load(bytes.NewReader(v))
+			if err != nil {
+				log.Warn().Msgf("read meta info: %s", err)
+				return nil
+			}
+
+			t, err = app.client.AddTorrent(mi)
+			if err != nil {
+				log.Warn().Msgf("add torrent: %s", err)
+				return nil
+			}
+
+			app.torrents[t.InfoHash().String()] = t
+
+			return nil
+		})
+	})
 }
 
 func (app *App) Client() *torrent.Client {
 	return app.client
 }
 
-func (app *App) Track(torrent *torrent.Torrent) {
+func (app *App) Track(t *torrent.Torrent) error {
 	app.mu.Lock()
-	app.torrents[torrent.InfoHash().String()] = torrent
-	app.mu.Unlock()
+	defer app.mu.Unlock()
+
+	var err error
+
+	app.torrents[t.InfoHash().String()] = t
+
+	var mi = t.Metainfo()
+	var buf = bytes.NewBuffer(nil)
+
+	err = mi.Write(buf)
+	if err != nil {
+		return fmt.Errorf("write metaInfo: %w", err)
+	}
+
+	err = app.db.Update(func(tx *bolt.Tx) error {
+		var b = tx.Bucket([]byte(dbBucketInfo))
+		return b.Put(t.InfoHash().Bytes(), buf.Bytes())
+	})
+	if err != nil {
+		return fmt.Errorf("put to db: %w", err)
+	}
+
+	return nil
 }
 
 func (app *App) Torrent(hash string) (*torrent.Torrent, bool) {
@@ -118,24 +159,17 @@ func (app *App) Close() error {
 	if app.tmp != "" {
 		err = os.RemoveAll(app.tmp)
 		if err != nil {
-			return err
+			return fmt.Errorf("remove temp. dir: %w", err)
+		}
+	}
+
+	// Close database.
+	if app.db != nil {
+		err = app.db.Close()
+		if err != nil {
+			return fmt.Errorf("close db: %w", err)
 		}
 	}
 
 	return nil
-}
-
-// https://gitlab.com/axet/libtorrent/-/blob/master/libtorrent.go
-func limit(kbps int) *rate.Limiter {
-	var l = rate.NewLimiter(rate.Inf, 0)
-
-	if kbps > 0 {
-		b := kbps
-		if b < 16*1024 {
-			b = 16 * 1024
-		}
-		l = rate.NewLimiter(rate.Limit(kbps), b)
-	}
-
-	return l
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/anacrolix/log v0.7.0
 	github.com/anacrolix/torrent v1.15.2
 	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535
+	github.com/boltdb/bolt v1.3.1
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/go-chi/cors v1.1.1
 	github.com/go-chi/render v1.0.1
